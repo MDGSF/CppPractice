65170c5f0d6c1f8a164f9725bd6709b62e32837b
HuangJian <1342042894@qq.com>
2021-05-22 17:21:50 +0800

    add cpptemplate.cpp

---
+++ cpptemplate.cpp
@@ -0,0 +1,25 @@
+// Copyright 2021 <HuangJian 1342042894@qq.com>
+#include <math.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <fstream>  // ifstream
+#include <functional>  // function object, bind
+#include <iostream>  // cin, cout, cerr
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>  // unique_ptr, shared_ptr, weak_ptr
+#include <set>
+#include <sstream>  // istringstream
+#include <string>
+#include <utility>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  cout << "hello world!" << endl;
+  return 0;
+}
