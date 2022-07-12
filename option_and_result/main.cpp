b558c8be11c6dc2ef8a605b1ee4a336d25d6eb53
HuangJian <1342042894@qq.com>
2022-07-12 17:15:52 +0800

    add option and result

---
+++ option_and_result/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+enum class OptionType { Some, None };
+
+template <typename T>
+struct Option {
+  OptionType type;
+  union {
+    struct {
+      T value;
+    };          // Some
+    struct {};  // None
+  };
+};
+
+enum class ResultType { Ok, Error };
+
+template <typename T, typename E>
+struct Result {
+  ResultType type;
+  union {
+    struct {
+      T value;
+    };  // Ok
+    struct {
+      E error;
+    };  // Error
+  };
+};
+
+int main() { return 0; }
